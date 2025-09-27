d962a76e930b727101680aa767c2f3ce79a33e27
moorts <moritz.gutfleisch@gmx.de>
2025-09-27 02:10:05 +0200

    Add home link to header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -5,9 +5,9 @@ export default function Header() {
   return (
     <header className="flex items-center justify-between p-4 bg-[#121212] text-green relative shadow-lg border-b border-black-800">
       {/* Website Title */}
-      <h1 className="text-2xl md:text-3xl font-black tracking-wider font-uncial drop-shadow-md text-[#DC143C]">
+      <Link href="/" className="text-2xl md:text-3xl font-black tracking-wider font-uncial drop-shadow-md text-[#DC143C]">
         A blog.
-      </h1>
+      </Link>
     </header>
   );
 }
