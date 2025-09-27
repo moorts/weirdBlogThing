42186daef218bb6584a24231fa111397650216a5
moorts <moritz.gutfleisch@gmx.de>
2025-09-27 02:09:45 +0200

    Add syntax highlighting

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -3,6 +3,11 @@ import { MDXRemote } from 'next-mdx-remote/rsc';
 import Title from '@/app/components/title.tsx';
 import { Tex } from '@/app/lib/math.tsx';
 import { notFound } from 'next/navigation';
+import rehypeStarryNight from 'rehype-starry-night';
+import rehypePrettyCode from 'rehype-pretty-code';
+import rehypeAddClasses from 'rehype-add-classes';
+
+import '@/app/code_colors.css';
 
 export default async function Page({
   params
@@ -21,7 +26,18 @@ export default async function Page({
   const title = frontmatter.title || id;
   return (<>
     <Title value={title} />
-    <MDXRemote source={content} components={{Tex: Tex}}/>
+    <MDXRemote
+      source={content}
+      components={{Tex: Tex}}
+      options={{
+        mdxOptions: {
+          rehypePlugins: [
+            rehypeStarryNight,
+            [rehypeAddClasses, { pre: "w-full bg-[#fffaf0] text-black p-2 rounded shadow-inner shadow-xl" }],
+          ]  
+        }
+      }}
+    />
   </>);
 }
 
